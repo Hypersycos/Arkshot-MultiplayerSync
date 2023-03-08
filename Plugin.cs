9fb80970f595fb4f1e25232da66c0061e1afc226
Joshua Pollick <39402728+Hypersycos@users.noreply.github.com>
2023-03-08 16:47:19 +0000

    Added property sync interface

--- Plugin.cs
+++ Plugin.cs
@@ -1,9 +1,11 @@
 ﻿using BepInEx;
+using BepInEx.Configuration;
 using ExitGames.Client.Photon;
 using HarmonyLib;
 using System.Collections.Generic;
 using System.Reflection;
 using System.Reflection.Emit;
+using UnityEngine;
 
 namespace MultiplayerSync
 {
@@ -19,27 +21,72 @@ namespace MultiplayerSync
 
         public class Tools
         {
-            static List<string> names;
-            static List<object> values;
-            public static Hashtable AddProperties(Hashtable roomProperties)
+            private static Dictionary<string, object> myValues = new();
+            private static Dictionary<string, object> hostValues = new();
+            private static int entryCount = 0;
+
+            public class SyncedEntry<T>
             {
-                for (int i = 0; i < names.Count; i++)
+                private string key;
+
+                private SyncedEntry(string key)
                 {
-                    roomProperties.Add(names[i], values[i]);
+                    this.key = key;
+                }
+
+                ~SyncedEntry()
+                {
+                    myValues.Remove(key);
+                }
+
+                public T Value
+                {
+                    get => (T)hostValues[key];
+                    set => myValues[key] = value;
+                }
+
+                internal static SyncedEntry<T> NewEntry(T value)
+                {
+                    myValues.Add(entryCount.ToString(), value);
+                    return new SyncedEntry<T>(entryCount++.ToString());
+                }
+            }
+            internal static Hashtable AddProperties(Hashtable roomProperties)
+            {
+                hostValues = new();
+                foreach(KeyValuePair<string, object> pair in myValues)
+                {
+                    roomProperties.Add(pair.Key, pair.Value);
+                    hostValues.Add(pair.Key, pair.Value);
                 }
                 return roomProperties;
             }
 
-            public static void RegisterProperty(BepInPlugin plugin, string property, object value)
+            internal static void SyncProperties(Hashtable roomProperties)
             {
-                names.Add(plugin.GUID + property);
-                values.Add(value);
+                hostValues = new();
+                foreach (var pair in roomProperties)
+                {
+                    hostValues.Add(pair.Key.ToString(), pair.Value);
+                }
             }
 
-            public static object GetProperty(BepInPlugin plugin, Hashtable roomProperties, string property)
+            public static SyncedEntry<T> RegisterSyncedValue<T>(T value)
+            {
+                return SyncedEntry<T>.NewEntry(value);
+            }
+
+            public static SyncedEntry<T> RegisterSyncedConfig<T>(ConfigEntry<T> configEntry)
+            {
+                SyncedEntry<T> toReturn = SyncedEntry<T>.NewEntry(configEntry.Value);
+                configEntry.SettingChanged += (_, _) => toReturn.Value = configEntry.Value;
+                return toReturn;
+            }
+
+            public static object GetProperty(Hashtable roomProperties, string property)
             {
                 object value = null;
-                roomProperties.TryGetValue(plugin.GUID + property, out value);
+                roomProperties.TryGetValue(property, out value);
                 return value;
             }
         }
@@ -52,7 +99,7 @@ namespace MultiplayerSync
 
             [HarmonyTranspiler]
             [HarmonyPatch("createRoom")]
-            static IEnumerable<CodeInstruction> injectProperties_Transpiler(IEnumerable<CodeInstruction> instructions)
+            static IEnumerable<CodeInstruction> injectPropertiesMultiplayer_Transpiler(IEnumerable<CodeInstruction> instructions)
             {
                 foreach (CodeInstruction instr in instructions)
                 {
@@ -61,6 +108,30 @@ namespace MultiplayerSync
                         yield return new CodeInstruction(OpCodes.Call, addValues);
                     }
                     yield return instr;
+                }
+            }
+
+            [HarmonyTranspiler]
+            [HarmonyPatch("Training")]
+            static IEnumerable<CodeInstruction> injectPropertiesTraining_Transpiler(IEnumerable<CodeInstruction> instructions)
+            {
+                foreach (CodeInstruction instr in instructions)
+                {
+                    if (instr.StoresField(customRoomProperties))
+                    {
+                        yield return new CodeInstruction(OpCodes.Call, addValues);
+                    }
+                    yield return instr;
+                }
+            }
+
+            [HarmonyPatch("OnJoinedRoom")]
+            [HarmonyPrefix]
+            static void onJoinedRoom_Prefix()
+            {
+                if (!PhotonNetwork.isMasterClient)
+                {
+                    Tools.SyncProperties(PhotonNetwork.room.customProperties);
                 }
             }
         }
