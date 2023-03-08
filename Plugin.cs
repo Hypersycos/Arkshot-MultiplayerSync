ec91af6842d9c1d3769869d1b272f69e18e54ea6
Joshua Pollick <39402728+Hypersycos@users.noreply.github.com>
2023-03-08 14:43:31 +0000

    First implementation

--- Plugin.cs
+++ Plugin.cs
@@ -1,14 +1,68 @@
 ﻿using BepInEx;
+using ExitGames.Client.Photon;
+using HarmonyLib;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
 
 namespace MultiplayerSync
 {
-    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    [BepInPlugin("hypersycos.plugins.arkshot.multiplayersync", "Multiplayer Sync", "0.1.0")]
     public class Plugin : BaseUnityPlugin
     {
+        public static Plugin Instance;
         private void Awake()
         {
             // Plugin startup logic
             Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        }
+
+        public class Tools
+        {
+            static List<string> names;
+            static List<object> values;
+            public static Hashtable AddProperties(Hashtable roomProperties)
+            {
+                for (int i = 0; i < names.Count; i++)
+                {
+                    roomProperties.Add(names[i], values[i]);
+                }
+                return roomProperties;
+            }
+
+            public static void RegisterProperty(BepInPlugin plugin, string property, object value)
+            {
+                names.Add(plugin.GUID + property);
+                values.Add(value);
+            }
+
+            public static object GetProperty(BepInPlugin plugin, Hashtable roomProperties, string property)
+            {
+                object value = null;
+                roomProperties.TryGetValue(plugin.GUID + property, out value);
+                return value;
+            }
+        }
+
+        [HarmonyPatch(typeof(mainMenu))]
+        public class Patches
+        {
+            static FieldInfo customRoomProperties = AccessTools.Field(typeof(RoomOptions), nameof(RoomOptions.customRoomProperties));
+            static MethodInfo addValues = SymbolExtensions.GetMethodInfo(() => Tools.AddProperties);
+
+            [HarmonyTranspiler]
+            [HarmonyPatch("createRoom")]
+            static IEnumerable<CodeInstruction> injectProperties_Transpiler(IEnumerable<CodeInstruction> instructions)
+            {
+                foreach (CodeInstruction instr in instructions)
+                {
+                    if (instr.StoresField(customRoomProperties))
+                    {
+                        yield return new CodeInstruction(OpCodes.Call, addValues);
+                    }
+                    yield return instr;
+                }
+            }
         }
     }
 }
